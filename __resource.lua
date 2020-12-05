3db89a6fce34d00abd7bf1262a5278d46bd2b655
starxtrem <83julcraft@gmail.com>
2020-12-06 00:49:54 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-description 'StarColor dev By starxtrem'
 
+author 'Starxtrem'
+description 'StarColor dev By starxtrem'
 version '1.0'
 
 client_scripts {
